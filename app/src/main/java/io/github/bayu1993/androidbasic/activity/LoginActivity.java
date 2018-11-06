2fdc5220e97f433c47d67f3dd83ac4ef2070a460
bayu <bayuteguhpamuji@gmail.com>
2018-11-06 14:19:40 +0700

    delete gmap api

--- app/src/main/java/io/github/bayu1993/androidbasic/activity/LoginActivity.java
+++ app/src/main/java/io/github/bayu1993/androidbasic/activity/LoginActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Patterns;
 import android.view.Gravity;
